bcf21260a982d73f2e615bf337543b82ab5a2ab1
mounika19004 <157041348+mounika19004@users.noreply.github.com>
2024-06-22 15:44:59 +0530

    Add files via upload

---
+++ lab.java
@@ -0,0 +1,35 @@
+import java.util.Random
+public class square extends thread{
+int n,a;
+void sq(int x){
+x=n;
+a=n*n;
+system.out.println("the square of number is:"=a)}
+}
+
+public classcube extends thread{
+int m,b;
+void cub(int y)
+{y=m;
+b=m*m*m;
+system.out.println("the cube of number is:"+b)}
+}
+
+public class lab 6{
+public static void main(String args[])
+Random random=new random[10];
+int Randomint=random.nextInt()
+s=sq(RandomInt);
+s.start();
+c=cub(Randomint);
+c.start()
+}
+try{threadsleep(1000)
+}
+catch
+{exception interrupt ex}
+
+
+Rnumber n=new Rnumber[]
+n.start
+
